6537e5471cedec6af519726efb74a5bf67e3573d
kosicheol <kosicheol@f6c13f83-189b-daf0-9b1d-8a2889825580>
2011-03-27 12:41:17 +0000

    문법오류 수정

--- trunk/jejunuBus/footer.php
+++ trunk/jejunuBus/footer.php
@@ -6,7 +6,7 @@ echo "현재 페이지를 예쁘게 꾸며주실분을 찾습니다.";
 echo "<br />";
 echo "이 서비스를 함께 발전 시켜나갈 분을 찾습니다.";
 echo "<br />";
-echo "<a href='http://code.google.com/p/jejunubus'>jejunubus</a>에서 프로젝트 진행 현황을 알수 있습니다."
+echo "<a href='http://code.google.com/p/jejunubus'>jejunubus</a>에서 프로젝트 진행 현황을 알수 있습니다.";
 echo "kosicheol@gmail.com로연락주세요.";
 echo "<br />";
 echo "<br />";

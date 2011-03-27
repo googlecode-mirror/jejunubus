b9ed05112b073c973d37ecf68eecd4827ed5a2ee
kosicheol <kosicheol@f6c13f83-189b-daf0-9b1d-8a2889825580>
2011-03-27 12:23:05 +0000

    프로젝트 생성

---
+++ jejunuBus/footer.php
@@ -0,0 +1,14 @@
+﻿<?php
+echo "<div id = 'information'>";
+echo "<a href='./checkDevice.php'><h3>현재 페이지를 <br />앱아이콘으로만들기</h3></a>";
+echo "<a href='./contributor.php'><h3>이 사람들이 만듭니다.</h3></a>";
+echo "현재 페이지를 예쁘게 꾸며주실분을 찾습니다.";
+echo "<br />";
+echo "이 서비스를 함께 발전 시켜나갈 분을 찾습니다.";
+echo "<br />";
+
+echo "kosicheol@gmail.com로연락주세요.";
+echo "<br />";
+echo "<br />";
+echo "</div>";
+?>

6d128f31aac838ccc8d64e1c1dd1955261302bec
kosicheol <kosicheol@f6c13f83-189b-daf0-9b1d-8a2889825580>
2011-03-28 00:16:05 +0000

    to update 1.0 Verson 업그레이드 를 위한 브랜칭, 테마기능, 현재시간을 이용한 버스 시간 추천기능 추가 예정

---
+++ trunk/jejunuBus1.0/checkDevice.php
@@ -0,0 +1,26 @@
+﻿<?php
+
+if(strstr($_SERVER['HTTP_USER_AGENT'],'iPhone') || strstr($_SERVER['HTTP_USER_AGENT'],'iPod')){
+
+echo "1. 화면 하단에 메뉴(?)버튼을 누릅니다.";
+echo "<img src='./img/webPage.jpg'/>";
+echo "<br />";
+echo "<br />";
+echo "2. 홈 화면에 추가 버튼을 누르세요.";
+echo "<br />";
+echo "<img src='./img/menu.jpg'/>";
+echo "<br />";
+echo "<br />";
+echo "3. 이름을 입력하고, 추가버튼을 누르세요.";
+echo "<br />";
+echo "<img src='./img/addPage.jpg'/>";
+echo "<br />";
+echo "<br />";
+echo "4. 웹페이지가 추가 되었습니다.";
+echo "<br />";
+echo "<img src='./img/iconList.jpg'/>";
+echo "<br />";
+
+echo "<br />";
+}
+?>

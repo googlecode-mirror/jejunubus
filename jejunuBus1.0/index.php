d77bcd128f9f795e87c54f29be24910b2a9dea37
sunghyun3580 <sunghyun3580@gmail.com>
2011-04-03 10:36:59 +0000

    javascript - bus_schedule.js, BusSchedule.js, prototype.js - bus_schedule.html

--- jejunuBus1.0/index.php
+++ jejunuBus1.0/index.php
@@ -3,7 +3,7 @@
 <title> 제주대 순환버스 for Smart phone</title>
  <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
  <meta name="viewport" content="width=deivce-width, initial-scale=1.0,maximum-scale=1.0,user-scalable=no" />
-  <link rel="stylesheet" type="text/css" href="./style.css"/>
+  <link rel="stylesheet" type="text/css" href="./style.css"/> 
 </head>
 <body>
 

937445d8643bdc1990a4906df0b68c3736abe39b
kosicheol <kosicheol@f6c13f83-189b-daf0-9b1d-8a2889825580>
2011-04-07 00:23:07 +0000

    앱 아이콘으로 만들었을때 보이는 아이콘 추가

--- jejunuBus1.0/index.php
+++ jejunuBus1.0/index.php
@@ -1,10 +1,11 @@
 ﻿<html>
 <head>
 <title> 제주대 순환버스 for Smart phone</title>
- <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
- <meta name="viewport" content="width=deivce-width, initial-scale=1.0,maximum-scale=1.0,user-scalable=no" />
-  <link rel="stylesheet" type="text/css" href="./css/defaultStyle.css"/> 
-  	<script type="text/javascript" src="./js/prototype.js"></script>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<meta name="viewport" content="width=deivce-width, initial-scale=1.0,maximum-scale=1.0,user-scalable=no" />
+	<link rel="shortcutIcon" href="./img/shortcutIcon.png"/>
+	<link rel="stylesheet" type="text/css" href="./css/defaultStyle.css"/> 
+	<script type="text/javascript" src="./js/prototype.js"></script>
 	<script type="text/javascript" src="./js/BusSchedule.js"></script>
 	<script type="text/javascript" src="./js/bus_schedule.js"></script>
 </head>

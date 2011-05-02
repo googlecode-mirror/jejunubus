d99dcd839bc27aa8f7e6d6a4610c90fcfc706ab0
kosicheol <kosicheol@f6c13f83-189b-daf0-9b1d-8a2889825580>
2011-05-02 14:06:54 +0000

    성현님 이름및 메일주소 추가

--- jejunuBus1.0/contributor.php
+++ jejunuBus1.0/contributor.php
@@ -1,11 +1,7 @@
 ﻿
-창설자(?) : 고시철 <br />
-e-mail : kosicheol@gmail.com <br />
-Twitter : @kosicheol <br />
-Facebook : Sicheol Ko <br />
-이성현
-
-강진혁
-
+창설자(?) : 고시철 kosicheol@gmail.com <br />
+<h5>프로젝트 참여자 </h5><br />
+이성현 sunghyun3580@gmail.com<br />
+강진혁 진혁님 이메일 적어주세요. ㅋㅋ<br />
 <br />
 디자인, 새로운 기능 제안, 기능구현 등을 통해 이름을 등록 할 수 있습니다.

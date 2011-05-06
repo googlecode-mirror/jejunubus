2a9439f7fd92ce49ca74bd525f5c4b593973b840
jinhyeok.kang@gmail.com <jinhyeok.kang@gmail.com@f6c13f83-189b-daf0-9b1d-8a2889825580>
2011-05-06 02:18:28 +0000

    E-mail

--- trunk/jejunuBus1.0/contributor.php
+++ trunk/jejunuBus1.0/contributor.php
@@ -2,6 +2,6 @@
 창설자(?) : 고시철 kosicheol@gmail.com <br />
 <h5>프로젝트 참여자 </h5><br />
 이성현 sunghyun3580@gmail.com<br />
-강진혁 진혁님 이메일 적어주세요. ㅋㅋ<br />
+강진혁 jinhyeok.kang@gmail.com<br />
 <br />
 디자인, 새로운 기능 제안, 기능구현 등을 통해 이름을 등록 할 수 있습니다.

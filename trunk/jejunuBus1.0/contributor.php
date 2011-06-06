6ab38a79f74a576a43245fba5faef0f320bd5c2f
kosicheol <kosicheol@f6c13f83-189b-daf0-9b1d-8a2889825580>
2011-06-06 16:25:02 +0000

    favicon 추가

--- trunk/jejunuBus1.0/contributor.php
+++ trunk/jejunuBus1.0/contributor.php
@@ -1,8 +1,9 @@
 ﻿<meta name="viewport" content="width=deivce-width, initial-scale=1.0,maximum-scale=1.0,user-scalable=no" />
 
-프로젝트 리더 : 고시철 kosicheol@gmail.com <br />
+<h4>프로젝트 리더</h4>
+ 고시철 kosicheol@gmail.com <br />
 <h4>프로젝트 참여자 </h4>
 이성현 sunghyun3580@gmail.com<br />
 강진혁 jinhyeok.kang@gmail.com<br />
 <br />
-디자인, 새로운 기능 제안, 기능구현 등을 통해 이름을 등록 할 수 있습니다.
+디자인, 새로운 기능 제안, 기능구현 등을 통해 프로젝트에 참여 할 수 있습니다.
--- trunk/jejunuBus1.0/index.php
+++ trunk/jejunuBus1.0/index.php
@@ -3,11 +3,15 @@
 <title> 제주대 순환버스 for Smart phone</title>
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 	<meta name="viewport" content="width=deivce-width, initial-scale=1.0,maximum-scale=1.0,user-scalable=no" />
-	<link rel="shortcutIcon" href="./img/shortcutIcon.png"/>
+	<meta name="apple-mobile-web-app-capable" content="yes" />
+	<meta name="apple-mobile-web-app-status-bar-style" content="black" />
+	<link rel="apple-touch-icon" href="./favicon.png"/>
+	<link rel="apple-touch-startup-image" href="./favicon.png">
+	<link rel="shortcutIcon" href="./favicon.png"/>
 	<link rel="stylesheet" type="text/css" href="./css/defaultStyle.css"/> 
 	<link rel="stylesheet" type="text/css" href="./css/DormitoryBus.css"/> 
 	<script type="text/javascript" src="./js/jquery-1.6.1.min.js"></script>
-	<script type="text/javascript" src="./js/DormitorybusJquery.js"></script>
+	<script type="text/javascript" src="./js/DormitoryBusJquery.js"></script>
 	
 </head>
 <body>
@@ -15,7 +19,7 @@
 
 
 $hour = date("Hi", time(0));
-if($hour < 1930){
+if($hour < 1930 && $hour > 220){
 	include_once './view/BusScheduleView.php';	
 }else{
 	include_once './view/DormitoryBusForFinalExam.php';	
@@ -23,14 +27,12 @@ if($hour < 1930){
 
 ?>
 
-
-
 <div id = 'information'>
 <h3><a href='./howToCreateShortcutIcon.php'>앱처럼 사용할래 </a></h3>
 <h3><a href='./contributor.php'>내가 만들었지롱 ㅋㅋ </a></h3>
 <h3><a href='./view/dormitoryCafeteria.php'>기숙사밥 </a></h3>
 <h3><a href='./view/UniversityCafeteria.php'>신관밥 </a></h3>
-<h3><a href='./board.php'>좋은아이디어 있어</a></h3>
+<h3><a href='./board.php'>의견게시판</a></h3>
 발 디자인에서 해방 시켜주실 후광을 발산하시는 분(디자이너), <br />
 손으로 코딩하는게 심심해서 발로 코딩하시는 분(프로그래머), <br />
 이 서비스에 접속자수를 폭발적으로 증가시킬수 있는 분(아이디어) , <br />

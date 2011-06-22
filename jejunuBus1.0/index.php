b60f3f4573c30c09bb5a28650ac254596c4b64e0
kosicheol <kosicheol@f6c13f83-189b-daf0-9b1d-8a2889825580>
2011-06-22 06:43:20 +0000

    weblog

--- jejunuBus1.0/index.php
+++ jejunuBus1.0/index.php
@@ -3,18 +3,31 @@
 <title> 제주대 순환버스 for Smart phone</title>
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 	<meta name="viewport" content="width=deivce-width, initial-scale=1.0,maximum-scale=1.0,user-scalable=no" />
-	<meta name="apple-mobile-web-app-capable" content="yes" />
-	<meta name="apple-mobile-web-app-status-bar-style" content="black" />
+<!--	<meta name="apple-mobile-web-app-capable" content="yes" />
+	<meta name="apple-mobile-web-app-status-bar-style" content="black" />-->
 	<link rel="apple-touch-icon" href="./favicon.png"/>
 	<link rel="apple-touch-startup-image" href="./favicon.png">
-	<link rel="shortcutIcon" href="./favicon.png"/>
+	
 	<link rel="stylesheet" type="text/css" href="./css/defaultStyle.css"/> 
 	<link rel="stylesheet" type="text/css" href="./css/DormitoryBus.css"/> 
 	<script type="text/javascript" src="./js/jquery-1.6.1.min.js"></script>
 	<script type="text/javascript" src="./js/DormitoryBusJquery.js"></script>
-	
+	<script type="text/javascript">
+
+	  var _gaq = _gaq || [];
+	  _gaq.push(['_setAccount', 'UA-22873643-1']);
+	  _gaq.push(['_trackPageview']);
+
+	  (function() {
+		var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+		ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+		var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+	  })();
+
+	</script>
 </head>
 <body>
+<a href=../jejunubus0.0/index.php id='oldVersionLink'>Old버전</a><br />
 <?php 
 
 
@@ -26,6 +39,8 @@ if($hour < 1930 && $hour > 220){
 }
 
 ?>
+
+
 
 <div id = 'information'>
 <h3><a href='./howToCreateShortcutIcon.php'>앱처럼 사용할래 </a></h3>

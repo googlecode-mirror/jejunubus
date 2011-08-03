1c5cb015bfd40f6cae246b4edb31fcf8260bacae
kosicheol <kosicheol@f6c13f83-189b-daf0-9b1d-8a2889825580>
2011-08-03 14:25:28 +0000

    css적용. 약간의 디자인.

--- jejunuBus2.0/application/views/main.php
+++ jejunuBus2.0/application/views/main.php
@@ -1,15 +1,11 @@
+<!DOCTYPE HTML>
 <html>
 <head>
-<title> 제주대 순환버스 for Smart phone</title>
-	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-	<meta name="viewport" content="width=deivce-width, initial-scale=1.0,maximum-scale=1.0,user-scalable=no" />
-<!--	<meta name="apple-mobile-web-app-capable" content="yes" />
-	<meta name="apple-mobile-web-app-status-bar-style" content="black" />-->
-	<link rel="apple-touch-icon" href="./favicon.png"/>
-	<link rel="apple-touch-startup-image" href="./favicon.png">
-	
-	<link rel="stylesheet" type="text/css" href="./css/defaultStyle.css"/> 
-	<link rel="stylesheet" type="text/css" href="./css/DormitoryBus.css"/> 
+<title>제주대 순환버스</title>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+	<meta name="viewport" content="width=deivce-width, initial-scale=1.0,maximum-scale=1.0,user-scalable=no">
+	<link rel="stylesheet" type="text/css" href="http://jejunubus.hosting.paran.com/css/common.css">
+	<link rel="stylesheet" type="text/css" href="http://jejunubus.hosting.paran.com/css/defaultStyle.css">
 </head>
 <body>
 <?php 
@@ -19,17 +15,28 @@ if($hour < 1930 && $hour > 220){
 }else{
 //	include_once './view/DormitoryBusForFinalExam.php';	
 }
+
 ?>
 
-<div id = 'information'>
-<!-- <h3><a href='library'>Library</a></h3> -->
-<h3><a href='contributor'>내가 만들었지롱 ㅋㅋ </a></h3>
-<h3><a href='dormitoryCafeteria'>기숙사밥 </a></h3>
-<h3><a href='universityCafeteria'>신관밥 </a></h3>
-<h3><a href='./board.php'>의견게시판</a></h3>
-<br />
-<a href='http://code.google.com/p/jejunubus'>code.google.com/p/jejunubus</a>에서 프로젝트에 대한 내용을 볼수 있습니다. <br />
+<div id='doc'>
+	<ul class="mainMenu content">
+		<li class="contributor"><a title="참여자정보" href='contributor'>내가 만들었지롱 ㅋㅋ</a></li>
+		<li class="dormitoryCafeteria"><a title="기숙사 식당 메뉴" href='dormitory/cafeteria'>기숙사식당메뉴 </a></li>
+		<li class="universityCafeteria"><a title="신관 식당 메뉴" href='university/cafeteria'>신관메뉴 </a></li>
+		<li class="opinionBoard"><a title="의견 게시판" href='./board.php'>의견게시판</a></li>
+	</ul>
+	<ul class="subMenu content">
+		<li><a title="프로젝트호스팅홈페이지" href='http://code.google.com/p/jejunubus'>프로젝트 홈페이지</a></li>
+	</ul>
+	<br>
+	<p style="padding-left:10px;">
+	ToDo: 의견 게시판작업,<br>
+	기숙사메뉴자동갱신작업,<br>
+	버스시간표작업(도서관, 학기중),<br>
+	JavaScript
+	</p>
+	
+	
 </div>
-
 </body>
 </html>

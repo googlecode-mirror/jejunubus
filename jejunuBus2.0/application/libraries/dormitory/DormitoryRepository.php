6696043d89eb6ac639ecaea88d28ce7a768acf4a
kosicheol <kosicheol@f6c13f83-189b-daf0-9b1d-8a2889825580>
2011-08-09 14:00:24 +0000

    기숙사 식당 메뉴 자동 업데이트 이전 작업 완료

---
+++ jejunuBus2.0/application/libraries/dormitory/DormitoryRepository.php
@@ -0,0 +1,39 @@
+<?php
+class DormitoryRepository{
+	var $ci;
+	
+	function __construct(){
+		$this->ci=&get_instance();
+		$this->ci->load->database('jejunubus');		
+	}
+	
+	function updateConnectionDate(){
+		$this->ci->db->query('UPDATE updateSchedule SET dormitory_updated = now() where id=1');
+	}
+	
+	function selectUpdatedDay(){
+		$query = $this->ci->db->query('SELECT dormitory_updated FROM updateSchedule LIMIT 1');
+		
+		return $query->row()->dormitory_updated;
+	}
+	
+	function deleteCafeteriaMenuTableAll(){
+		$this->ci->db->query('DELETE FROM dormitoryMeal');
+	}
+	
+	function insertMenuTable($menuTable){
+		$mealIndex = array('date'=>0, 'earlyMorning'=>1, 'breakfast'=>2, 'lunch'=>3, 'dinner'=>4);
+		$dormitoryMeal = "dormitoryMeal";
+		
+		foreach ($menuTable as $meals){
+		
+		$this->ci->db->query("INSERT INTO 
+			{$dormitoryMeal} (menuDate, earlyMorning, breakfast, lunch, dinner)
+			VALUES ('{$meals[$mealIndex['date']]}', 
+					'{$meals[$mealIndex['earlyMorning']]}', 
+					'{$meals[$mealIndex['breakfast']]}', 
+					'{$meals[$mealIndex['lunch']]}', 
+					'{$meals[$mealIndex['dinner']]}')");
+		}
+	}
+}

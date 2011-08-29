97ba429cb0c66543bdba2f6118b74b53e92220e8
kosicheol <kosicheol@f6c13f83-189b-daf0-9b1d-8a2889825580>
2011-08-29 13:52:29 +0000

    개강 후 버스시간 등록및 2.0 배포준비 완료. 게시판 임시로 1.0 버전에 리다이렉트, 버스시간 추천해주는 자바스크립 작성

--- jejunuBus2.0/application/views/main.php
+++ jejunuBus2.0/application/views/main.php
@@ -1,35 +1,67 @@
 <!DOCTYPE HTML>
 <html>
 	<head>
-	<title>제주대 순환버스</title>
+	<title>제주대순환버스</title>
 		<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-		<meta name="viewport" content="width=deivce-width, initial-scale=1.0,maximum-scale=1.0,user-scalable=no">
+		<meta name="viewport" content="width=deivce-width, initial-scale=1.0, maximum-scale=1.0,user-scalable=no">
 		<link rel="stylesheet" type="text/css" href="http://jejunubus.hosting.paran.com/css/common.css">
 		<link rel="stylesheet" type="text/css" href="http://jejunubus.hosting.paran.com/css/defaultStyle.css">
 	</head>
 	<body>
 		<div id='doc'>
-			<ul class="specialContent">
-				<?php foreach ($contents as $content):?>
-				<li><?php echo $content;?></li>
-				<?php endforeach;?>
-			</ul>
+			<div id='bus'>
+			<?php if(isset($contents['up'])){?>
+				<div id='up'>
+					<h5>정문-></h5>
+					<ul id="goUpBus">
+						<?php foreach ($contents['up'] as $content):?>
+						<li<?php echo " value=".$content?>><?php printf("%02d : %02d", $content/100, $content%100);?></li>
+						<?php endforeach;?>
+					</ul>
+				</div>
+				<div id='down'>
+					<h5><-해대</h5>
+					<ul id="goDownBus">
+						<?php foreach ($contents['down'] as $content):?>
+						<li<?php echo " value=".$content?>><?php printf("%02d : %02d", $content/100, $content%100);?></li>
+						<?php endforeach;?>
+					</ul>
+				</div>
+			<?php } elseif(isset($contents['dormi'])){?>
+				<div id="dormi">
+					<h5>365일 언제나 중도버스</h5>
+					<ul>
+						<li class='on'>24 : 05</li>
+					</ul>
+				</div>
+			<?php }?>
+			</div>
+			<script type="text/javascript" src="http://jejunubus.hosting.paran.com/js/bus.js"></script>
+			<script type="text/javascript">
+				
+				shortenBusSchedule("goUpBus");
+				shortenBusSchedule("goDownBus");
+				
+			</script>
+			<div class="cls">
+			</div>
 			<ul class="mainMenu content">
-				<li class="contributor"><a title="참여자정보" href='contributor'>내가 만들었지롱 ㅋㅋ</a></li>
-				<li class="dormitoryCafeteria"><a title="기숙사 식당 메뉴" href='dormitory/cafeteria'>기숙사식당메뉴 </a></li>
-				<li class="universityCafeteria"><a title="신관 식당 메뉴" href='university/cafeteria'>신관메뉴 </a></li>
-				<li class="opinionBoard"><a title="의견 게시판" href='./board.php'>의견게시판</a></li>
+				<li class="contributor">
+					<a title="참여자정보" href='contributor'>내가 만들었지롱 ㅋㅋ</a>
+				</li>
+				<li class="dormitoryCafeteria">
+					<a title="기숙사 식당 메뉴" href='dormitory/cafeteria'>기숙사식당메뉴 </a>
+				</li>
+				<li class="universityCafeteria">
+					<a title="신관 식당 메뉴" href='university/cafeteria'>신관메뉴 </a>
+				</li>
+				<li class="opinionBoard">
+					<a title="의견 게시판" href='mention'>의견게시판</a>
+				</li>
 			</ul>
 			<ul class="subMenu content">
 				<li><a title="프로젝트호스팅홈페이지" href='http://code.google.com/p/jejunubus'>프로젝트 홈페이지</a></li>
 			</ul>
-			<br>
-			<p style="padding-left:10px;">
-			ToDo: 의견 게시판작업,<br>
-			기숙사메뉴자동갱신작업,<br>
-			버스시간표작업(도서관, 학기중),<br>
-			JavaScript
-			</p>	
 		</div>
 	</body>
 </html>

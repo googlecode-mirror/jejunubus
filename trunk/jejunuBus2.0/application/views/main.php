ad5117a5caa68b34cbddcdffe1239575e52b45aa
kosicheol <kosicheol@f6c13f83-189b-daf0-9b1d-8a2889825580>
2011-09-01 11:04:30 +0000

    google Web Log analsys javascript code added

--- trunk/jejunuBus2.0/application/views/main.php
+++ trunk/jejunuBus2.0/application/views/main.php
@@ -6,6 +6,18 @@
 		<meta name="viewport" content="width=deivce-width, initial-scale=1.0, maximum-scale=1.0,user-scalable=no">
 		<link rel="stylesheet" type="text/css" href="http://jejunubus.hosting.paran.com/css/common.css">
 		<link rel="stylesheet" type="text/css" href="http://jejunubus.hosting.paran.com/css/defaultStyle.css">
+		<script type="text/javascript">
+		  var _gaq = _gaq || [];
+		  _gaq.push(['_setAccount', 'UA-22873643-1']);
+		  _gaq.push(['_trackPageview']);
+	
+		  (function() {
+			var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+			ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+			var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+		  })();
+	
+		</script>
 	</head>
 	<body>
 		<div id='doc'>

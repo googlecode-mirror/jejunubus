26b110934642965ffe5b34f938332f082a281d30
odyody12 <odyody12@f6c13f83-189b-daf0-9b1d-8a2889825580>
2011-09-02 12:04:29 +0000

    edit jejucitybus page

--- trunk/jejunuBus2.0/application/views/jeju_citybus.php
+++ trunk/jejunuBus2.0/application/views/jeju_citybus.php
@@ -16,15 +16,32 @@
 	</head>
 	<body>
 		<div id='doc'>
+		<?php
+		$currentTime = date('Hi');
+		
+		$currentHour = $currentTime / 100;
+		$currentHour = number_format($currentHour, 0,`.`,`,`); //소수점 0자리로
+		
+		$currentMinute = $currentTime % 100;
+		
+		echo '현재시간 : '.$currentHour.'시 '.$currentMinute.'분'; 
+		?>
+		<br>
+		<br>
+		<h5>버스 출발 20분 전까지 표시됩니다.</h5>
+		<br>
+		<hr>
 		<ul id="top">
 			<li>버스번호 </li>
 			<li>출발시간</li>
 			<li>목적지</li>
 			<li>비고</li>
-		</ul>	
+		</ul>
+		<br>	
+		<hr>
 		<div id="clr"></div>
 		<?php foreach ($citytime->result_array() as $bustime):?>
-		<h5> </h5>
+		
 	  	<ul id="body">
 			<li><?php echo $bustime['busNo'].'번'; ?> </li>
 			<li><?php echo (($bustime['time']) - date('Hi')).'분전'; ?> </li>
@@ -32,6 +49,14 @@
 			<li><?php echo $bustime['etc']; ?> </li>
 		</ul>
 		<?php endforeach;?>
+		<br>
+		<hr>
+		<br>
+		<h5>제대 정문에서 출발하는 버스 시간표입니다</h5>
+		<br>
+		<h5>시내버스 시간표를 가지고 만든 페이지이기 때문에</h5>
+		<br>
+		<h5>100% 정확하지 않을 수 있습니다.</h5>
 	</div>
 	</body>
 </html>

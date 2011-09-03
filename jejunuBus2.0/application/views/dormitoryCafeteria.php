901679431bfbf9e1bf159732d1dc140eb3a35f6c
kosicheol <kosicheol@f6c13f83-189b-daf0-9b1d-8a2889825580>
2011-09-03 02:50:41 +0000

    당일과 다음 날의 식단을 보여주도록 javascript추가

--- jejunuBus2.0/application/views/dormitoryCafeteria.php
+++ jejunuBus2.0/application/views/dormitoryCafeteria.php
@@ -3,21 +3,40 @@
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
 <meta name="viewport" content="width=deivce-width, initial-scale=1.0,maximum-scale=1.0,user-scalable=no" />
-<link rel="stylesheet" type="text/css" href="http://117.17.102.67/css/dormitoryCafeteria.css"/>
-<link rel="stylesheet" type="text/css" href="http://117.17.102.67/css/common.css"/>
+<link rel="stylesheet" type="text/css" href="http://jejunubus.hosting.paran.com/test/css/dormitoryCafeteria.css"/>
+<link rel="stylesheet" type="text/css" href="http://jejunubus.hosting.paran.com/css/common.css"/>
 </head>
 
 <body>
 	<div id='doc'>
-		<?php foreach ($menu->result_array() as $meal):?>
-		<h5><?php echo $meal['menuDate']; ?> </h5>
-	  	<ul>
-			<li><?php echo "조식 : ".$meal['earlyMorning']; ?> </li>
-			<li><?php echo "아침 : ".$meal['breakfast']; ?> </li>
-			<li><?php echo "점심 : ".$meal['lunch']; ?> </li>
-			<li><?php echo "저녁 : ".$meal['dinner']; ?> </li>
-		</ul>
-		<?php endforeach;?>
+		<div id='menu'>
+			<?php foreach ($menu->result_array() as $meal):?>
+			<div>
+				<h5><?php echo $meal['menuDate']; ?> </h5>
+			  	<ul>
+					<li><?php echo "조식 : ".$meal['earlyMorning']; ?> </li>
+					<li><?php echo "아침 : ".$meal['breakfast']; ?> </li>
+					<li><?php echo "점심 : ".$meal['lunch']; ?> </li>
+					<li><?php echo "저녁 : ".$meal['dinner']; ?> </li>
+				</ul>
+			</div>
+			<?php endforeach;?>
+		</div>
+		<script type="text/javascript">
+			function shortenMenu(id){
+				var date = new Date();
+				var weekDayIndex = date.getDay();
+				weekDayIndex = weekDayIndex == 0 ? 7: weekDayIndex;
+				weekDayIndex--;
+								
+				var div = document.getElementById(id);
+				var week = div.getElementsByTagName('div');
+
+				week[weekDayIndex].className = "on";
+				week[weekDayIndex == 6 ? 0: weekDayIndex+1].className = "on";
+			}
+			shortenMenu('menu');
+		</script>
 		<h5>평 일</h5>
 		<ul>
 			<li> 조기 : 07:00 - 07:30(빵,우유)</li>
@@ -36,7 +55,7 @@
 			<li> 아침 : 07:30 - 09:00</li>
 			<li> 저녁 : 17:30 - 19:00</li>
 		</ul>
-		<iframe id='hidden' src = 'http://localhost/zend/jejunubus/jejunubus2.0/index.php/update/dormitory/cafeteria'>
+		<iframe id='hidden' src = '../update/dormitory/cafeteria'>
 		</iframe>
 	</div>
 </body>

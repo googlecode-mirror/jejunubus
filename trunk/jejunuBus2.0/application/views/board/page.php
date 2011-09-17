66b451c2529c18773f3425eb8695ed21e45b08b0
jinhyeok.kang <jinhyeok.kang@f6c13f83-189b-daf0-9b1d-8a2889825580>
2011-09-17 12:10:17 +0000

    게시판 페이징 링크 경로 수정

--- trunk/jejunuBus2.0/application/views/board/page.php
+++ trunk/jejunuBus2.0/application/views/board/page.php
@@ -1,9 +1,9 @@
 <div id="pageform">
 	<div class="backpage">
-		<a href="http://jejunubus.hosting.paran.com/jejunubus2.0/index.php/board/page/<?php echo $backpage;?>"> < </a>
+		<a href="<?php echo $backpage;?>"> < </a>
 	</div>
 	<div class="nextpage">
-		<a href="http://jejunubus.hosting.paran.com/jejunubus2.0/index.php/board/page/<?php echo $nextpage;?>"> > </a>
+		<a href="<?php echo $nextpage;?>"> > </a>
 	</div>
 	<div> <?php echo $page;?> </div>
 </div>

b3ed914db6b68d1d75dd9a5ac9dc9625061173ab
jinhyeok.kang <jinhyeok.kang@f6c13f83-189b-daf0-9b1d-8a2889825580>
2011-09-17 13:23:37 +0000

    페이징 수정....(main에서가 문제있었네요.. )

--- jejunuBus2.0/application/views/board/page_back.php
+++ jejunuBus2.0/application/views/board/page_back.php
@@ -1,6 +1,6 @@
 <div id="pageform">
 	<div class="backpage">
-		<a href="page/<?php echo $backpage;?>"> < </a>
+		<a href="<?php echo $backpage;?>"> < </a>
 	</div>
 	<div> <?php echo $page;?> </div>
 </div>

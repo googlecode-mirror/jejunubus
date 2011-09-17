51d30d3b16a15b2f4c98d3481f16042569ed01a4
jinhyeok.kang <jinhyeok.kang@f6c13f83-189b-daf0-9b1d-8a2889825580>
2011-09-17 13:20:02 +0000

    페이징 수정

--- jejunuBus2.0/application/views/board/page_next.php
+++ jejunuBus2.0/application/views/board/page_next.php
@@ -1,6 +1,6 @@
 <div id="pageform">
 	<div class="nextpage">
-		<a href="<?php echo $nextpage;?>"> > </a>
+		<a href="page/<?php echo $nextpage;?>"> > </a>
 	</div>
 	<div> <?php echo $page;?> </div>
 </div>

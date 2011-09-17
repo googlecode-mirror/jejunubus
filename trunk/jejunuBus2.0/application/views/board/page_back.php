e00df30eb2351bf42d3f69260a79ccad7d4d2160
jinhyeok.kang <jinhyeok.kang@f6c13f83-189b-daf0-9b1d-8a2889825580>
2011-09-17 11:50:30 +0000

    jejunubus 1.0버전 게시판 페이지 버그 수정 jejunubus 2.0버전 으로 게시판 수정작업 완료 (댓글 기능은 추가 예정)

---
+++ trunk/jejunuBus2.0/application/views/board/page_back.php
@@ -0,0 +1,6 @@
+<div id="pageform">
+	<div class="backpage">
+		<a href="http://jejunubus.hosting.paran.com/jejunubus2.0/index.php/board/page/<?php echo $backpage;?>"> < </a>
+	</div>
+	<div> <?php echo $page;?> </div>
+</div>
---
+++ trunk/jejunuBus2.0/application/views/board/page_next.php
@@ -0,0 +1,6 @@
+<div id="pageform">
+	<div class="nextpage">
+		<a href="http://jejunubus.hosting.paran.com/jejunubus2.0/index.php/board/page/<?php echo $nextpage;?>"> > </a>
+	</div>
+	<div> <?php echo $page;?> </div>
+</div>

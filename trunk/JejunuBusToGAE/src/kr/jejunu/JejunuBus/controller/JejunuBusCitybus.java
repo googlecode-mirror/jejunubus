395bb485942d642f4f41334fa197c58ae573e63a
odyody12@gmail.com <odyody12@gmail.com@f6c13f83-189b-daf0-9b1d-8a2889825580>
2011-12-18 09:35:43 +0000

    구글 앱엔진<Spring framework> 버젼.

---
+++ trunk/JejunuBusToGAE/src/kr/jejunu/JejunuBus/controller/JejunuBusCitybus.java
@@ -0,0 +1,13 @@
+package kr.jejunu.JejunuBus.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class JejunuBusCitybus {
+	@RequestMapping("/citybus")
+	public String action() {
+		
+		return "";
+	}
+}

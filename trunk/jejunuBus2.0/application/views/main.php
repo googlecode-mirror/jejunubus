556bd066ee4fe8724326522d3c5a522bd58ba24b
kosicheol@gmail.com <kosicheol@gmail.com@f6c13f83-189b-daf0-9b1d-8a2889825580>
2012-03-17 08:46:10 +0000

    merge jejunubus2.1[UI project] into jejunubus2.0

--- trunk/jejunuBus2.0/application/views/main.php
+++ trunk/jejunuBus2.0/application/views/main.php
@@ -3,21 +3,12 @@
 	<head>
 	<title>제주대순환버스</title>
 		<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-		<meta name="viewport" content="width=deivce-width, initial-scale=1.0, maximum-scale=1.0,user-scalable=no">
-		<link rel="stylesheet" type="text/css" href="http://jejunubus.hosting.paran.com/css/common.css">
-		<link rel="stylesheet" type="text/css" href="http://jejunubus.hosting.paran.com/test/css/defaultStyle.css">
-		<script type="text/javascript">
-		  var _gaq = _gaq || [];
-		  _gaq.push(['_setAccount', 'UA-22873643-1']);
-		  _gaq.push(['_trackPageview']);
-	
-		  (function() {
-			var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-			ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-			var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-		  })();
-	
-		</script>
+		<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0,user-scalable=no">
+		<link rel="stylesheet" type="text/css" href="http://jejunubus.hosting.paran.com/css/mainView.css">
+		
+		<script src="http://jejunubus.hosting.paran.com/js/googleAnalytics.js"></script>
+		
+		
 	</head>
 	<body>
 		<div id='doc'>
@@ -67,9 +58,11 @@
 						<?php endforeach;?>
 					</ul>
 				</div>
-				<script type="text/javascript" src="http://jejunubus.hosting.paran.com/js/bus.js"></script>
+				<script src="http://jejunubus.hosting.paran.com/js/touchScroller.js"></script>
 				<script type="text/javascript">
-				var hyosungToggle = new Secret({target:'bus',idSet:{goUpBus:'goUpBus', goDownBus:'goDownBus'}});
+				var touchScrollableDivId = "bus";
+				touchScroll(touchScrollableDivId);
+			
 				</script>
 			<?php }else {?>
 				<div id="lib">
@@ -79,11 +72,26 @@
 						<li class=' on'><?php printf("%02d : %02d", $bus/100, $bus%100);?></li>
 						<?php endforeach;?>
 					</ul>
+						<script>
+							var aa = document.getElementById('libBus');
+							console.log(aa);
+							var aas = aa.children;
+								</script>
 					<ul id='wayPoint'>
 						<li><span class='bold'>구제주방면=></span>중앙도서관→염광APT→아라주공APT→아라동사무소→제주여고→중앙여고→법원→구세무서→시청→한국통신→구신중→구신고→양돈조합→중앙여중→농협중앙회→동문로터리→동초등교→제주여상→이도2동사무소→인화동(구행복예식장)→인화초등교→문예회관→삼성의원(종점)</li>
-						<li><span class='bold'>02:05 버스(삼양,화북추가구간)=></span>인화동(구행복예식장)→삼화아파트→오현고정류소→화북남문→화북주공→삼양초등학교→인화초등교</li>
-						<li><span class='bold'>신제주방면=></span>중앙도서관→법원→시청→터미널→건설회관→신제주로터리→한라병원→노형로터리→부영아파트사거리(종점)</li>
+						<li><span class='bold'>(셤기간)02:05 버스(삼양,화북추가구간)=></span>인화동(구행복예식장)→삼화아파트→오현고정류소→화북남문→화북주공→삼양초등학교→인화초등교</li>
+						<li><span class='bold'>(셤기간)신제주방면=></span>중앙도서관→법원→시청→터미널→건설회관→신제주로터리→한라병원→노형로터리→부영아파트사거리(종점)</li>
 					</ul>
+					<script>
+						var bb = document.getElementById('wayPoint');
+							var bbs = bb.children;
+							var path = "";
+							console.log(bbs.length, bbs[0].textContent);
+							for(var i=0; i<bbs.length; i++){
+								path += bbs[i].textContent+"\n\n";
+							}
+							aas[0].onclick = function(){alert(path);};
+					</script>
 				</div>
 				<script type="text/javascript" src="http://jejunubus.hosting.paran.com/js/libBus.js"></script>
 				<script type="text/javascript">
@@ -91,11 +99,9 @@
 				</script>
 			<?php }?>
 			</div>
-			<div class="cls">
-			</div>
-			<ul class="mainMenu content">
+			<ul class="mainMenu">
 				<li class="contributor">
-					<a title="참여자정보" href='contributor'>내가 만들었지롱 ㅋㅋ</a>
+					<a title="참여자정보" href='contributor'>참여자정보</a>
 				</li>
 				<li class="dormitoryCafeteria">
 					<a title="기숙사 식당 메뉴" href='dormitory/cafeteria'>기숙사식당메뉴 </a>
@@ -104,16 +110,23 @@
 					<a title="신관 식당 메뉴" href='university/cafeteria'>신관메뉴 </a>
 				</li>
 				<li class="dormitoryCafeteria">
-					<a title="제주 시내버스 출발 시간표" href='jeju_citybus'>제대 출발 시내버스</a>
+					<a id='citybus' title="제주 시내버스 출발 시간표" href='jeju_citybus'>제대출발 시내버스</a>
+					<script>
+					var date = new Date();
+					var hhmm = date.getHours();
+					hhmm = hhmm+""+date.getMinutes();
+					var citybus = document.getElementById('citybus');
+					citybus.href = citybus.href+'?clientTime='+hhmm;
+					</script>
 				</li>
-				<li class="universityCafeteria">
+				<li class="universityDomitory">
 					<a title="제대 도서관" href='librarie/meetingRoom'>중도나이트</a>
 				</li>
 				<li class="opinionBoard">
 					<a title="의견 게시판" href='board/page/1'>의견게시판</a>
 				</li>
 			</ul>
-			<ul class="subMenu content">
+			<ul class="subMenu">
 				<li><a title="프로젝트호스팅홈페이지" href='http://code.google.com/p/jejunubus'>프로젝트 홈페이지</a></li>
 			</ul>
 		</div>

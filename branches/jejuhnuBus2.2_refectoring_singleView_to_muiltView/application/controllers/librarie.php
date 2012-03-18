50646608f11b994232846b685f761f476683ebd7
kosicheol@gmail.com <kosicheol@gmail.com@f6c13f83-189b-daf0-9b1d-8a2889825580>
2012-03-18 04:58:19 +0000

    Created branch jejuhnuBus2.2_refectoring_singleView_to_muiltView.

---
+++ branches/jejuhnuBus2.2_refectoring_singleView_to_muiltView/application/controllers/librarie.php
@@ -0,0 +1,54 @@
+<?php
+class librarie extends CI_Controller {
+
+	function librarie() {
+		parent :: __construct();
+
+	}
+	function index(){
+		$this->search();
+	}
+	
+	function search(){
+		$keyword = "";
+		$page = 1;
+		
+		if(isset($_GET['keyword'])){
+			$keyword = $_GET['keyword'];
+			$keyword = urlencode($keyword);
+		}
+		if(isset($_GET['page'])){
+			$page = $_GET['page'];
+		}
+		if($keyword == ""){
+			$this->load->view('librarie');
+		}else{
+			$this->searchBook($keyword, $page);
+		}
+	}
+	
+	function meetingRoom(){
+		$this->load->view('meetingRoom');
+	}
+	
+	function searchBook($keyword, $page){
+		$this->load->library('librarie/LibrarieService');
+		
+		//$keyword = "ssat";
+		
+		$bookType = "1^2^56^57";//cl_id 단행본에서만 검색 전자자료등은 파싱하는 데이터 형식이 다름.(위치없음.)
+		
+		$numberOfBookInPage = 15;
+		$searchOption = "m_var=421&srv_id=31&qy_frm=QUICK&adf=&adt=&cl_id=".$bookType."&rid_all=&st_f=&st_o=&pg=".$page."&rpp=".$numberOfBookInPage."&mc=300&dp_op=LIST&t_path=&qy_typ=KEYWORD&brch=01&qy_idx=TITL&qy_kwd=".$keyword;
+		$service = new LibrarieService();
+		$books = $service->requestBookSearch($searchOption);
+		
+		$data['books'] = $books;
+		$prevPage = $page < 2 ? 1 : $page - 1;
+		$data['prevPage'] = $prevPage;
+		$data['nextPage'] = $page = $page + 1;
+		$data['keyword'] = $keyword; 
+		$this->load->view('librarie', $data);
+	}
+}
+?>
---
+++ branches/jejuhnuBus2.2_refectoring_singleView_to_muiltView/application/views/board/feed.php
@@ -0,0 +1,4 @@
+<div class="viewform"> 
+	<div class="name"> 작성자 : <?php echo $name;?> </div>
+	<div class="content"> <?php echo $content;?> </div>
+</div>

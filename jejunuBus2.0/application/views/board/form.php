6e36c9dc80cb56abd4703061ee5522e38d8acefe
kosicheol <kosicheol@gmail.com>
2012-04-19 04:13:16 +0000

    폼 validtion JavaScript적용.

--- jejunuBus2.0/application/views/board/form.php
+++ jejunuBus2.0/application/views/board/form.php
@@ -3,7 +3,7 @@
 <head>
 	<title>제주대 순환버스 for Smart phone</title>
 	<meta http-equiv=Content-Type content=text/html; charset=utf-8 />
-	<meta name="viewport" content="width=deivce-width, initial-scale=1.0, maximum-scale=1.0,user-scalable=no" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0,user-scalable=no" />
 	<link rel=shortcutIcon href=./img/shortcutIcon.png />
 	<link rel=stylesheet type=text/css href=http://jejunubus.hosting.paran.com/jejunubus1.0/css/board.css />
 </head>
@@ -15,10 +15,16 @@
 				<input id=author class=name type=text name=title value=작성자> 
 				<input class=submit type=submit value=Submit>
 			</form>
-			<script type="text/javascript" src="http://jejunubus.hosting.paran.com/js/board.js"></script> 
+			<script type="text/javascript" src="http://jejunubus.hosting.paran.com/js/formValid.js"></script> 
 			<script type="text/javascript">
-				clearText('comment');	
-				clearText('author');
+				var validForm = valid();
+				var author = document.getElementById('author');
+				var comment = document.getElementById('comment');
+				var btn = document.getElementsByClassName('submit')[0];
+				var validFormat = new Array({ele:comment, defaultText:'좋은 의견 많이주세요^^'},
+											{ele:author, defaultText:'작성자'});
+				validForm.setDefultText(validFormat);
+				btn.onclick = validForm.onValid(validFormat);
 			</script>
 		</div>
 	

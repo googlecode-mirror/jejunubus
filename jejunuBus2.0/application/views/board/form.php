aa75e56cf0d8d96c581aa8f0bc45f4000ed524f9
kosicheol <kosicheol@gmail.com>
2012-04-29 10:26:33 +0000

    이전버전의 css참조를 css/board.css를 참조하도록 수정

--- jejunuBus2.0/application/views/board/form.php
+++ jejunuBus2.0/application/views/board/form.php
@@ -5,7 +5,7 @@
 	<meta http-equiv=Content-Type content=text/html; charset=utf-8 />
 	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0,user-scalable=no" />
 	<link rel=shortcutIcon href=./img/shortcutIcon.png />
-	<link rel=stylesheet type=text/css href=http://jejunubus.hosting.paran.com/jejunubus1.0/css/board.css />
+	<link rel="stylesheet" type="text/css" href="http://jejunubus.hosting.paran.com/css/board.css">
 </head>
 <body>
 	<div id=boardform>

deadfbfb2b3e2bcb3b69bd90eb038d0e674a3b7c
kosicheol@gmail.com <kosicheol@gmail.com@f6c13f83-189b-daf0-9b1d-8a2889825580>
2012-05-10 11:38:06 +0000

    도서관 열람실 좌석 수정 글자 깨지지 않도록 열람실 리스트를 따로 관리해서 클릭하면 iframe속에서 바뀌도록 수정 아이폰에서의 가로 모드와 세로 모드에서 잘보이도록 수

--- trunk/jejunuBus2.0/application/views/meetingRoom.php
+++ trunk/jejunuBus2.0/application/views/meetingRoom.php
@@ -3,37 +3,20 @@
 <head>
 <title>제주대 순환버스</title>
 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-	<meta name="viewport" content="initial-scale=0.3, minimum-scale=0.28, maximum-scale=2.0, user-scalable=yes">
-	<link rel="stylesheet" type="text/css" href="http://jejunubus.hosting.paran.com/css/common.css">
+	<meta name="viewport" content="user-scalable=yes">
 	<style type="text/css">
-		iframe {height:600px; border:none; background-color:blue;}
-		.aa {position:absolute; width:100%; font-size:2.5em; font-weight:bold;}
-		.aa {margin-bottom:20%;}
-		.aa li{float:right; margin:0 1%;}
+		iframe {border:none; height:600px;}
+		.roomList {font-size:2.5em; font-weight:bold;}
+		.roomList {margin-bottom:20%;}
+		.roomList li{float:right; list-style:none; margin:0 1% 8% 1%;}
 		
-		#doc {background-color:red;}
+		
 	</style>
 </head>
 <body>
-	<script>
-		var supportsOrientationChange = "onorientationchange" in window,
-			orientationEvent = supportsOrientationChange ? "orientationchange" : "resize";
-		window.addEventListener("onresize", function(){
-			console.log('resize', window.width);
-		});
-		window.addEventListener(orientationEvent, function  () {
-			console.log(document.width);
-			var isLandscape = true;
-			if(isLandscape){
-				var viewPort = document.getElementsByName('viewport')[0].content = "initial-scale=0.4";
-				console.log(viewPort);
-			}
-		  //document.getElementsByName('viewport')[0].content="initial-scale=0.4";
-		})	
-	</script>
 	<div id='doc'>
 		<iframe id='resultBox' src='http://203.253.194.57/MAPTEMP/1_180.182.28.169.html'></iframe>
-		<ul class='aa'>
+		<ul class='roomList'>
 			<li><a href='http://203.253.194.57/MAPTEMP/1_180.182.28.169.html'>1열람실</a></li>
 			<li><a href='http://203.253.194.57/MAPTEMP/2_180.182.28.169.html'>2열람실</a></li>
 			<li><a href='http://203.253.194.57/MAPTEMP/6_180.182.28.169.html'>3열람실</a></li>
@@ -50,7 +33,6 @@
 			
 			var list = document.getElementsByTagName('a');
 			for(var i=0; i<list.length; i++){
-				console.log(i, list[i]);
 				list[i].onclick = function(){showResultToIframe(this); return false;};
 			}
 		</script>

8d5985d1e97f366bd8139588c48ef41e56d49a32
kosicheol@gmail.com <kosicheol@gmail.com@f6c13f83-189b-daf0-9b1d-8a2889825580>
2012-05-27 13:04:04 +0000

    큰화면에서 신관메뉴, 도서관자리 제대로 안보이는 문제수정(width:100%적용)

--- trunk/jejunuBus2.0/application/views/universityCafeteria.php
+++ trunk/jejunuBus2.0/application/views/universityCafeteria.php
@@ -5,6 +5,10 @@
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=0.43,maximum-scale=2.0,user-scalable=yes">
 	<link rel="stylesheet" type="text/css" href="../../resource/css/common.css">
+	<style>
+		iframe {width:100%; height:100%;}
+		
+	</style>
 </head>
 <body>
 	<div id='doc'>

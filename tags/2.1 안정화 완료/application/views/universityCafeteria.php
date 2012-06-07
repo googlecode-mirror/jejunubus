4fe73efeba2e1d2a946b3e337695563b6aa9c275
kosicheol@gmail.com <kosicheol@gmail.com@f6c13f83-189b-daf0-9b1d-8a2889825580>
2012-06-07 13:44:26 +0000

    안정화 완

---
+++ tags/2.1 안정화 완료/application/views/universityCafeteria.php
@@ -0,0 +1,18 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+<title>제주대 순환버스</title>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=0.43,maximum-scale=2.0,user-scalable=yes">
+	<link rel="stylesheet" type="text/css" href="../../resource/css/common.css">
+	<style>
+		iframe {width:100%; height:100%;}
+		
+	</style>
+</head>
+<body>
+	<div id='doc'>
+		<iframe src=http://www.jejunu.ac.kr/enboard/boardread.jsp?id=11&no=1245154925704&fname=viewFrame></iframe>
+	</div>
+</body>
+</html>
